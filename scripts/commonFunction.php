08a3a323f260d3098dbc52e902d6be959cc782d7
Payhemfoh <56668252+Payhemfoh@users.noreply.github.com>
2020-07-30 13:47:46 +0800

    Add files via upload

---
+++ scripts/commonFunction.php
@@ -0,0 +1,12 @@
+<?php
+    function returnToHomepage(){
+        echo "<br><br>";
+        echo "<center>";
+        echo "Please press the button below to return to homepage.";
+        echo "<form action=\"Homepage.php\">";
+        echo "<input type=\"submit\" value=\"go\">";
+        echo "</form>";
+        echo "</center>";
+    }
+
+?>
