c44db723f68cae1a718b01ec2c6b80c838d1fa82
Payhemfoh <56668252+Payhemfoh@users.noreply.github.com>
2020-08-02 12:45:22 +0800

    Update commonFunction.php

--- scripts/commonFunction.php
+++ scripts/commonFunction.php
@@ -3,7 +3,17 @@
         echo "<br><br>";
         echo "<center>";
         echo "Please press the button below to return to homepage.";
-        echo "<form action=\"Homepage.php\">";
+        echo "<form action=\"../webpage/homepage.php\">";
+        echo "<input type=\"submit\" value=\"go\">";
+        echo "</form>";
+        echo "</center>";
+    }
+
+    function returnToLogin(){
+        echo "<br><br>";
+        echo "<center>";
+        echo "Please press the button below to login.";
+        echo "<form action=\"../webpage/login.php\">";
         echo "<input type=\"submit\" value=\"go\">";
         echo "</form>";
         echo "</center>";
